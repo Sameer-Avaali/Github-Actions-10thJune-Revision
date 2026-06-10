48646712a1b884b0f1b6623cbcdf28c2df7e0535
Sameer-Avaali <sameer.shariff@avaali.com>
2026-06-10 11:23:07 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 
-const PORT = 5050;
+const PORT = 3000;  // match docker
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL = "mongodb://admin:qwerty@mongo:27017";
 const client = new MongoClient(MONGO_URL);
 
-//GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
-
-    const db = client.db("apnacollege-db");
+    await client.connect();
+    const db = client.db("Test-db");
     const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
+    await client.close();
     res.send(data);
 });
 
-//POST new user
 app.post("/addUser", async (req, res) => {
     const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
-
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
-    client.close();
+    await client.connect();
+    const db = client.db("Test-db");
+    await db.collection('users').insertOne(userObj);
+    await client.close();
+    res.send("User Added");
 });
 
-
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
